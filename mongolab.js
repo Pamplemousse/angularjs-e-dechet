c0b02cc8db56ce788ad59b1998faec67c701b447
xav.maso <xav.maso@gmail.com>
2013-06-01 14:43:36 +0200

    ebauche projet e-dechet avec angularjs
    recuperation de donnees sur mongolab
    
    vue principale

---
+++ mongolab.js
@@ -0,0 +1,25 @@
+angular.module('mongolab', ['ngResource']).
+    factory('Garbage', function($resource) {
+      var Garbage = $resource('https://api.mongolab.com/api/1/databases/angularjs/collections/garbages/:id',
+                      { apiKey: 'xum_xn6Xn7QcL4K03_H9fj-Ks0H4lH3T' }, { add: {method: 'POST'}, update: {method: 'PUT'} }
+                    );
+
+      // Project.prototype.add = function(cb) {
+      //   return Project.add({id: this._id.$oid},
+      //       angular.extend({}, this, {_id:undefined}), cb);
+      // };
+
+      return Garbage;
+    }).
+    factory('Category', function($resource) {
+      var Category = $resource('https://api.mongolab.com/api/1/databases/angularjs/collections/categories/:id',
+                      { apiKey: 'xum_xn6Xn7QcL4K03_H9fj-Ks0H4lH3T' }, { add: {method: 'POST'}, update: {method: 'PUT'} }
+                    );
+      return Category;
+    }).
+    factory('Room', function($resource) {
+      var Room = $resource('https://api.mongolab.com/api/1/databases/angularjs/collections/rooms/:id',
+                      { apiKey: 'xum_xn6Xn7QcL4K03_H9fj-Ks0H4lH3T' }, { add: {method: 'POST'}, update: {method: 'PUT'} }
+                    );
+      return Room;
+    });
