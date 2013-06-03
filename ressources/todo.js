2c23dd755b0f82e92936e719ea25af55a54bdf46
xav.maso <xav.maso@gmail.com>
2013-06-03 19:23:12 +0200

    ajout bibliothèque select2

--- ressources/todo.js
+++ ressources/todo.js
@@ -12,11 +12,20 @@ function GarbageCtrl($scope, $location, $routeParams, Garbage) {
       self.original = garbage;
       $scope.garbage = new Garbage(self.original);
     });
-
 }
 
 
 function ListCtrl($scope, $location, Garbage) {
+  $scope.$on('$viewContentLoaded', function() {
+                                      $("#e2").select2({
+                                          placeholder: "Select a State",
+                                          allowClear: true
+                                      });
+                                      $("#e2_2").select2({
+                                          placeholder: "Select a State"
+                                      });
+                                    });
+
   $scope.garbages = Garbage.query();
 
   $scope.goTo = function(garbageId) {
