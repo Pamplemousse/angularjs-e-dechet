f9d60c53ef08c5b1c411bf08ea588b2fc7ba9f18
Pamplemousse <xav.maso@gmail.com>
2013-06-18 16:36:14 +0200

    mise à jour du mock des catégories

--- mocks/categories.js
+++ mocks/categories.js
@@ -1,16 +1,54 @@
-   var mock_categories = [{
-           "name": "laine",
-       }, {
-           "name": "verre",
-       }, {
-           "name": "plastique",
-       }, {
-           "name": "carton",
-       }, {
-           "name": "aluminium",
-       }, {
-           "name": "cuir",
-       }, {
-           "name": "coton",
-       }
+   var mock_categories = [
+      {
+        "name" : "plastique",
+        "value" : ""
+      },
+      {
+        "name" : "papier-carton",
+        "value" : ""
+      },
+      {
+        "name" : "métal",
+        "value" : ""
+      },
+      {
+        "name" : "verre",
+        "value" : ""
+      },
+      {
+        "name" : "bois",
+        "value" : ""
+      },
+      {
+        "name" : "textile",
+        "value" : ""
+      },
+      {
+        "name" : "déchets dangereux",
+        "value" : ""
+      },
+      {
+        "name" : "gravats",
+        "value" : ""
+      },
+      {
+        "name" : "végétaux",
+        "value" : "" 
+      },
+      {
+        "name" : "alimentaire",
+        "value" : ""
+      },
+      {
+        "name" : "électronique",
+        "value" : ""
+      },
+      {
+        "name" : "électrique",
+        "value" : ""
+      }
    ];
+
+
+
+
