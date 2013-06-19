806fddefc0fce364332a745b8f5bfa85d302dba1
ranska <go@rubyrock.me>
2013-06-19 11:03:17 +0200

    implement ui-map

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 'use strict';
 
-angular.module('project', ['filters','mongolab','ui.select2']).
+angular.module('project', ['filters','mongolab','ui.select2','ui.map','ui.event']).
   config(function($routeProvider) {
     $routeProvider.
       when('/', {controller:ListCtrl, templateUrl:'app/partials/list.html'}).
+      when('/map', {controller:MapCtrl, templateUrl:'app/partials/map.html'}).
       when('/garbage/:garbageId', {controller:GarbageCtrl, templateUrl:'app/partials/garbage.html'}).
       otherwise({redirectTo:'/'});
   });
+
+
