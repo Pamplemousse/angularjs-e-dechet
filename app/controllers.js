e7b4b37c2777f436c239f4eaeea0b238f652f298
Pamplemousse <xav.maso@gmail.com>
2013-06-19 16:38:36 +0200

    un peu de css (moche)

--- app/controllers.js
+++ app/controllers.js
@@ -31,39 +31,35 @@ function GarbageCtrl($scope, $location, $routeParams, Garbage) {
   Garbage.get({id: $routeParams.garbageId}, function(garbage) {
     self.original = garbage;
     $scope.garbage = new Garbage(self.original);
-
-
-
-
-    // On remplit à l'arrache les lieux susceptibles d'accueillir ce déchet (sans doublons)
-    $scope.locations = new Array();
-    var random = new Array();
-    for (var i=0 ; i<5 ; i++) {
-      var test = Math.floor(Math.random() * 42);
-      while ($.inArray(test, random) != -1){
-        test = Math.floor(Math.random() * 42);
-      }
-      random[i] = test;
-    }
-    for (var i=0 ; i<5 ; i++) {
-      $scope.locations[i] = mock_locations[random[i]];
-    }
-
-    for (i in $scope.locations) {
-      $scope.locations[i].address = ($scope.locations[i].address == undefined || $scope.locations[i].address == '')? "Pas d'adresse enregistrée pour la : "+$scope.locations[i].name : $scope.locations[i].address;
-    }
-
-    $scope.oneAtATime = false;
-
-
-
-
-
   });
-  var ll = new google.maps.LatLng(13.0810, 80.2740);
+
+  // On remplit à l'arrache les lieux susceptibles d'accueillir ce déchet (sans doublons)
+  $scope.locations = new Array();
+  var random = new Array();
+  for (var i=0 ; i<5 ; i++) {
+    var test = Math.floor(Math.random() * 42);
+    while ($.inArray(test, random) != -1){
+      test = Math.floor(Math.random() * 42);
+    }
+    random[i] = test;
+  }
+  for (var i=0 ; i<5 ; i++) {
+    $scope.locations[i] = mock_locations[random[i]];
+  }
+
+  for (i in $scope.locations) {
+    $scope.locations[i].address = ($scope.locations[i].address == undefined || $scope.locations[i].address == '')? "Pas d'adresse enregistrée pour la : "+$scope.locations[i].name : $scope.locations[i].address;
+  }
+
+  // Pour l'accordéon
+  $scope.oneAtATime = false;
+
+
+  // Un point de la map arbitraire : la position de l'utilisateur
+  var ll = new google.maps.LatLng(44.966389, -0.883056);
   $scope.mapOptions = {
     center: ll,
-    zoom: 15,
+    zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
@@ -71,8 +67,8 @@ function GarbageCtrl($scope, $location, $routeParams, Garbage) {
   $scope.onMapIdle = function() {
     if ($scope.myMarkers === undefined){    
       var marker = new google.maps.Marker({
-	map: $scope.myMap,
-	position: ll
+      	map: $scope.myMap,
+      	position: ll
       });
       $scope.myMarkers = [marker, ];
     }
